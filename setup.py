907a520fae8995be01b69b436ba35e13c8dbeaeb
yaniv <yaniv.hamou@gmail.com>
2025-05-18 22:16:08 +0200

    amake the utils folder to work

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='utils', version='0.1', packages=['src/utils'])
+setup(
+    name="utils",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "PyGithub==2.2.0",
+        "python-dotenv==1.0.1",
+        "requests>=2.23.0,<2.27.2",  # Compatible with redshift-connector
+        "tqdm==4.66.2",
+        "pyarrow>=2.0.0,<7.1.0",  # Compatible with awswrangler
+        "boto3>=1.24.21,<1.25.0",  # Compatible with aiobotocore
+        "charset-normalizer>=2.0,<3.0"  # Compatible with aiohttp
+    ],
+)
